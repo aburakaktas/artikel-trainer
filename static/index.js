3d85d9c3f0068c596ea68338ae154791c1d9cce9
Burak Aktas <aburakaktas@gmail.com>
2022-11-15 08:00:27 +0100

    kind of works on js at this stage I am going to fuck it up from here probably. I will try to add post request for every word answer.

--- static/index.js
+++ static/index.js
@@ -1,6 +1,7 @@
 var id = 1;
 var score = 0;
 var wordInfo = getWordInfo(words, id);
+var answers = {};
 
 // get word info by providing the words array and a word id
 function getWordInfo(arr, id) {
@@ -23,18 +24,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     dasButton = document.getElementById("das");
-
     dasButton.addEventListener('click', function () {
         var result = "das".localeCompare(wordInfo.article);
         console.log(result);
         if (result == 0) {
             console.log("Das was correct");
+            dasButton.style.backgroundColor = ('green');
+            dasButton.style.transition = "all 2s"
+            
             score++;
+            answers[wordInfo.word_id] = "true";
+            
         }
         else {
             console.log("Das was incorrect");
+            document.getElementById("das").style.backgroundColor = ('red');
+            answers[wordInfo.word_id] = "false";
         }
+        
         id++;
+        console.log(answers);
         wordInfo = getWordInfo(words, id)
         displayWord(wordInfo);
     });
