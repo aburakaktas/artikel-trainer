42152e5f41b40b994b3ae1dc57771f885d8d1ab4
Burak Aktas <aburakaktas@gmail.com>
2022-12-18 20:18:49 +0100

    Managed to change the default font to Inter. Only took 4 hours ¯\_(ツ)_/¯

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode:'jit',
+  mode: 'jit',
   content: ["./templates/**/*.{html,js}"],
   theme: {
-    extend: {},
+    
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+      colors: {
+        primary: '#C44E00',
+        primary_hover: '#993D00',
+        card_bg: '#fdf3eb',
+        page_bg: '#ff9ba6',
+        button_bg: '#efe1de',
+        button_bg_hover: '#dec1ba',
+        primary: '#c34e00',
+        primary_hover: '#993d00',
+      }
+    },
   },
   plugins: [],
 }
